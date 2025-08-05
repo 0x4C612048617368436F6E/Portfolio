aae85120f0b173c66b2a7a50203633e1af09124c
Benjamin <benjaminenadeba@gmail.com>
2025-08-05 03:05:22 +0100

    main data

---
+++ client/data.js
@@ -0,0 +1,74 @@
+export const data = [
+  { label: "Welcome to La HashCon Portfolio" },
+  { label: "About:" },
+  [
+    { label: "Documentation:" },
+    { hint: "Type 'documentation' to see documentation" },
+  ],
+  [{ label: "Support:" }, { hint: "Type 'support' to see support" }],
+  { label: `System information as of ${new Date().toString()}` },
+  { label: "hostname:" },
+  { label: "OS Type:" },
+  { label: "Uptime:" },
+  { label: "Architecture:" },
+  { label: "System load:" },
+  { label: "processes:" },
+  { label: "IP Address:" },
+];
+
+export const logoFile = [
+  { 0: "3kom-superhack.txt" },
+  { 1: "cow-branded-longhorn.txt" },
+  { 2: "cow-head.txt" },
+  { 3: "cowsay.txt" },
+  { 4: "ctf202.txt" },
+  { 5: "figlet.txt" },
+  { 6: "gargoyle.hwtxt" },
+  { 7: "ghost01.hwtxt" },
+  { 8: "haKCers.txt" },
+  { 9: "help-using-a-module.txt" },
+  { 10: "honk.txt" },
+  { 11: "i-heart-shells.txt" },
+  { 12: "json01.hwtxt" },
+  { 13: "metasploit-heart-red-blood.txt" },
+  { 14: "metasploit-heart-red.txt" },
+  { 15: "metasploit-park.txt" },
+  { 16: "metasploit-shield.txt" },
+  { 17: "metasploit-trail.txt" },
+  { 18: "metasploit-v5.txt" },
+  { 19: "missile-command.txt" },
+  { 20: "mummy-hwtxt" },
+  { 21: "ninja.txt" },
+  { 22: "null-pointer-deref.txt" },
+  { 23: "petagram01.hwtxt" },
+  { 24: "pony-01.aftxt" },
+  { 25: "pony-02.aftxt" },
+  { 26: "pony-03.aftxt" },
+  { 27: "pony-04.aftxt" },
+  { 28: "pony-05.aftxt" },
+  { 29: "pumkin01.hwtxt" },
+  { 30: "pumkin02.hwtxt" },
+  { 31: "pumkin03.hwtxt" },
+  { 32: "pumkin04.hwtxt" },
+  { 33: "r7-metasploit.txt" },
+  { 34: "tricks01.hwtxt" },
+  { 35: "wake-up-neo.txt" },
+  { 36: "workflow.txt" },
+  { 37: "zsploit-1.txt" },
+  { 38: "zsploit-2.txt" },
+  { 39: "zsploit-3.txt" },
+];
+
+export const customMetasploitAnsiEscapemapper = [
+  { "%clr": "" }, //probably clear
+  { "%whi": "" }, //probably white
+  { "%yel": "" }, //probably yellow
+  { "%red": "" }, //probably red
+  { "%bld": "" }, //probably bold
+  { "%blu": "" }, //probably blue
+  { "%mag": "" }, //not sure
+  { "%dred": "" }, //not sure
+  { "%grn": "" }, //probably green
+  { "%cya": "" }, //not sure
+  { "%blk": "" }, //probably black
+];
